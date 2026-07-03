2896375c0de655914808f38c06e4f66afae3d6d7
parthasaradhi <nunepardhu05@gmail.com>
2026-07-03 18:34:10 +0530

    Support MONGO_URI and MONGODB_URI db connection variables

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/sleep_clarity');
+    const conn = await mongoose.connect(process.env.MONGO_URI || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sleep_clarity');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
